f6488eafb9b0d982f96f89a2912b18a41bf2bd9c
Sergey Tatsiak <tatsiak@gmail.com>
2018-10-10 13:46:59 +0300

    delete browser defining, add browserType parameter

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ import ChromeMessenger from "./browser-messagers/ChromeMessenger";
 import NormalExtensionsMessenger from "./browser-messagers/NormalExtensionsMessenger";
 import SafariMessenger from "./browser-messagers/SafariMessenger";
 
-var chrome = window.chrome || false;
-var safari = window.safari || false;
-
-var browserType = "";
-if (chrome) {
-  browserType = "chrome";
-}
-if (safari) {
-  browserType = "safari";
-}
-if (browser) {
-  browserType = "firefoxOrEdge"
-}
-if (browserType === ""){
-  console.error("web-extension-messenger not support your type of extension");
-}
-
 /**
  * type definition
  * @typedef {object} Implementations
@@ -27,108 +10,112 @@ if (browserType === ""){
  * @property {object} safari - Safari implementations
  * @property {object} firefoxOrEdge - Firefox and Edge implementations
  */
-const Implementations = {
-  chrome: ChromeMessenger,
-  safari: SafariMessenger,
-  firefoxOrEdge: NormalExtensionsMessenger,
-  "": class NoClass {}
-}[browserType];
 
-export default class Messenger extends Implementations {
-  constructor() {
-    super();
-    this.callbacks = [];
-    this.responses = {};
-  }
 
-  /**
-   * Send message to tab
-   * @param {object} tab
-   * @param {object}  message
-   * @return {void}
-   */
-  sendMessageToTab(tab, message) {
-    super.sendMessageToTab(tab, message);
-  }
+export default function (browserType) {
+  const Implementations = {
+    chrome: ChromeMessenger,
+    safari: SafariMessenger,
+    firefoxOrEdge: NormalExtensionsMessenger,
+    "": class NoClass {}
+  }[browserType];
 
-  /**
-   * Send message to tab and get promise with response
-   * @param {tab} tab
-   * @param {object} message
-   * @param {number} timeToWaitResponse time in ms what we wait response
-   * @return {Promise<object>}
-   */
-  sendMesssageToTabAndGetResponse(tab, message, timeToWaitResponse = 500) {
-    if (!message.messId) {
-      message.messId = guid();
+  return class Messenger extends Implementations {
+    constructor() {
+      super();
+      this.callbacks = [];
+      this.responses = {};
     }
 
-    return new Promise((resolve, reject) => {
-      var timer = null;
-      this.responses[message.messId] = function (response) {
-        clearTimeout(timer);
-        resolve(response);
-      };
-
-      timer = setTimeout(() => {
-        delete this.responses[message.messId];
-        reject();
-      }, timeToWaitResponse);
-
-      this.sendMessageToTab(tab, message);
-    });
-  }
-
-  /**
-   * Push callback in array of callbacks
-   * @param {function} callback
-   * @return {void}
-   */
-  onMessage(callback) {
-    this.callbacks.push(callback);
-  }
-
-  /**
-   * Remove callback from array of callbacks
-   * @param {function} callback
-   * @return {void}
-   */
-  offMessage(callback) {
-    this.callbacks = this.callbacks.filter(cb => cb !== callback);
-  }
-
-  /**
-   * send message to other messengers
-   * @param {object} message
-   */
-  sendMessage(message) {
-    super.sendMessage(message);
-  }
-
-  /**
-   *
-   * @param {object} message to send
-   * @param {number} timeToWaitResponse time in ms what we wait response
-   * @return {Promise<object>} with response
-   */
-  sendMessageAndGetResponse(message, timeToWaitResponse = 500) {
-    if (!message.messId) {
-      message.messId = guid();
+    /**
+     * Send message to tab
+     * @param {object} tab
+     * @param {object}  message
+     * @return {void}
+     */
+    sendMessageToTab(tab, message) {
+      super.sendMessageToTab(tab, message);
     }
 
-    return new Promise((resolve, reject) => {
-      var timer = null;
-      this.responses[message.messId] = function (response) {
-        clearTimeout(timer);
-        resolve(response);
-      };
+    /**
+     * Send message to tab and get promise with response
+     * @param {tab} tab
+     * @param {object} message
+     * @param {number} timeToWaitResponse time in ms what we wait response
+     * @return {Promise<object>}
+     */
+    sendMesssageToTabAndGetResponse(tab, message, timeToWaitResponse = 500) {
+      if (!message.messId) {
+        message.messId = guid();
+      }
 
-      timer = setTimeout(() => {
-        delete this.responses[message.messId];
-        reject();
-      }, timeToWaitResponse);
+      return new Promise((resolve, reject) => {
+        var timer = null;
+        this.responses[message.messId] = function (response) {
+          clearTimeout(timer);
+          resolve(response);
+        };
 
-      this.sendMessage(message);
-    });
+        timer = setTimeout(() => {
+          delete this.responses[message.messId];
+          reject();
+        }, timeToWaitResponse);
+
+        this.sendMessageToTab(tab, message);
+      });
+    }
+
+    /**
+     * Push callback in array of callbacks
+     * @param {function} callback
+     * @return {void}
+     */
+    onMessage(callback) {
+      this.callbacks.push(callback);
+    }
+
+    /**
+     * Remove callback from array of callbacks
+     * @param {function} callback
+     * @return {void}
+     */
+    offMessage(callback) {
+      this.callbacks = this.callbacks.filter(cb => cb !== callback);
+    }
+
+    /**
+     * send message to other messengers
+     * @param {object} message
+     */
+    sendMessage(message) {
+      super.sendMessage(message);
+    }
+
+    /**
+     *
+     * @param {object} message to send
+     * @param {number} timeToWaitResponse time in ms what we wait response
+     * @return {Promise<object>} with response
+     */
+    sendMessageAndGetResponse(message, timeToWaitResponse = 500) {
+      if (!message.messId) {
+        message.messId = guid();
+      }
+
+      return new Promise((resolve, reject) => {
+        var timer = null;
+        this.responses[message.messId] = function (response) {
+          clearTimeout(timer);
+          resolve(response);
+        };
+
+        timer = setTimeout(() => {
+          delete this.responses[message.messId];
+          reject();
+        }, timeToWaitResponse);
+
+        this.sendMessage(message);
+      });
+    }
   }
 }
