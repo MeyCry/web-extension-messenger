b3d64605d9b9fdd79f6a92064c10ca9f950fd9c4
meycry <timchukvadim@gmail.com>
2020-11-03 16:58:00 +0200

    improve types 2

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-export type ExtensionMessage<P, T> = {
+export type ExtensionMessage<P = any, T = string> = {
   status?: number;
   messId?: string;
   type: T;
   payload: P;
 };
 
-export type onMessageCallbackType<T, P> = (message: ExtensionMessage<P, T>, tab?: Record<string, any>) => void;
+export type onMessageCallbackType<M extends ExtensionMessage> = (message: M, tab?: chrome.tabs.Tab | browser.tabs.Tab) => void;
 
 export class Messenger {
   sendMessage<T>(message: T): void;
@@ -14,8 +14,8 @@ export class Messenger {
   sendMessageToTabAndGetResponse<T1, T2>(tab: chrome.tabs.Tab | browser.tabs.Tab, message: T1, timeToWaitResponse: number): Promise<T2>;
   sendMessageAndGetResponse<T1, T2>(message: T1, timeToWaitResponse: number): Promise<T2>;
   sendMessageAndGetResponseGlobal<T1, T2>(message: T1, timeToWaitResponse: number): Promise<T2>;
-  onMessage<T, P>(cb: onMessageCallbackType<T, P>): void;
-  offMessage<T, P>(cb: onMessageCallbackType<T, P>): void;
+  onMessage<M extends ExtensionMessage>(cb: onMessageCallbackType<M>): void;
+  offMessage<M extends ExtensionMessage>(cb: onMessageCallbackType<M>): void;
 }
 
 export default function(browserType: string): typeof Messenger;
